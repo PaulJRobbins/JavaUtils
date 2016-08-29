8829f17338d777bd8d84b7a7ce551a6b877f33e7
PaulJRobbins <paulrobbins@Huitzilopochtli.local>
2016-08-29 16:38:14 -0500

    Added more unit tests and added JavaDocs.

--- src/main/java/com/robbins/paul/utils/JavaUtils.java
+++ src/main/java/com/robbins/paul/utils/JavaUtils.java
@@ -6,7 +6,6 @@ import java.util.List;
 import java.util.stream.Collectors;
 import java.util.stream.DoubleStream;
 import java.util.stream.IntStream;
-import java.util.stream.LongStream;
 
 public final class JavaUtils {
 
@@ -14,42 +13,71 @@ public final class JavaUtils {
 		throw new IllegalAccessError("Utility Class");
 	}
 
+	/*
+	 * @param low int representing the low end of the range,inclusive
+	 * @param high int representing the high end of the range,inclusive
+	 * @return a list of Integers between low and high, inclusive
+	 */
 	public static List<Integer> getRangeAsList(int low, int high) {
 		return IntStream.rangeClosed(low, high).boxed().collect(Collectors.toList());
 	}
 
+	/*
+	 * @param nums A list of integers to be totaled
+	 * @return the total of all integers in list nums
+	 */
 	public static int total(List<Integer> nums) {
 		return nums.stream().mapToInt(Integer::intValue).sum();
 	}
 
+	/*
+	 * @param nums an array of integers to be totaled
+	 * @return the total of all integers in array nums
+	 */
 	public static int total(int[] nums) {
 		return IntStream.of(nums).sum();
 	}
 
-	public static long total(long[] nums) {
-		return LongStream.of(nums).sum();
-	}
-
+	/*
+	 * @param nums an array of doubles to be totaled
+	 * @return the total of all doubles in array nums
+	 */
 	public static double total(double[] nums) {
 		return DoubleStream.of(nums).sum();
 	}
 
-
+	/*
+	 * @param nums an array of integers to be converted to a list
+	 * @return an ArrayList of the Integers in array nums
+	 */
 	public static List<Integer> arrayToList(int[] nums) {
 		return Arrays.stream(nums).boxed().collect(Collectors.toList());
 
 	}
 
+	/*
+	 * @param nums an array of doubles to be converted to a list
+	 * @return an ArrayList of the Doubles in array nums
+	 */
 	public static List<Double> arrayToList(double[] nums) {
 		return Arrays.stream(nums).boxed().collect(Collectors.toList());
 
 	}
 
+	/*
+	 * @param ints a List of Integers to find multiples from
+	 * @param ...mults any number of multiples to be found
+	 * @return all multiples of ...mults in the List ints
+	 */
 	public static List<Integer> getMultiplesInList(List<Integer> ints, int... mults) {
 
 		return ints.stream().filter(n -> IntStream.of(mults).anyMatch(k -> n % k == 0)).collect(Collectors.toList());
 	}
 
+	/*
+	 * @param nums a List of Integers to be converted to an array
+	 * @return an array of the ints in List nums
+	 */
 	public static int[] intListToArray(List<Integer> nums) {
 		int[] ints = new int[nums.size()];
 		Iterator<Integer> iterator = nums.iterator();
@@ -60,6 +88,10 @@ public final class JavaUtils {
 		return ints;
 	}
 
+	/*
+	 * @param nums a List of Doubles to be converted to an array
+	 * @return an array of the doubles in List nums
+	 */
 	public static double[] doubleListToArray(List<Double> nums) {
 		double[] doubles = new double[nums.size()];
 		Iterator<Double> iterator = nums.iterator();
@@ -69,47 +101,35 @@ public final class JavaUtils {
 		
 		return doubles;
 	}
-
-	public static long[] longListToArray(List<Long> nums) {
-		long[] longs = new long[nums.size()];
-		Iterator<Long> iterator = nums.iterator();
-		for(int i = 0; i < nums.size(); i++) {
-			longs[i] = iterator.next().longValue();
-		}
-		
-		return longs;
-	}
-
-	public static short[] shortListToArray(List<Short> nums) {
-		short[] shorts = new short[nums.size()];
-		Iterator<Short> iterator = nums.iterator();
-		for(int i = 0; i < nums.size(); i++) {
-			shorts[i] = iterator.next().shortValue();
-		}
-		
-		return shorts;
-	}
 	
+	/*
+	 * @param nums an array of integers to extract evens from
+	 * @return an array of integers containing all even ints in nums
+	 */
 	public static int[] getEvensFromArray(int[] nums) {
 		return Arrays.stream(nums).filter(n -> n%2 == 0).toArray();
 	}
 
-	public static long[] getEvensFromArray(long[] nums) {
-		return Arrays.stream(nums).filter(n -> n%2 == 0).toArray();
-	}
-
+	/*
+	 * @param nums an array of doubles to extract evens from
+	 * @return an array of doubles containing all even doubles in nums
+	 */
 	public static double[] getEvensFromArray(double[] nums) {
 		return Arrays.stream(nums).filter(n -> Math.abs(n%2) <= 0.000000001).toArray();
 	}
 
+	/*
+	 * @param nums an array of integers to extract odds from
+	 * @return an array of integers containing all odd ints in nums
+	 */
 	public static int[] getOddsFromArray(int[] nums) {
 		return Arrays.stream(nums).filter(n -> n%2 != 0).toArray();
 	}
 
-	public static long[] getOddsFromArray(long[] nums) {
-		return Arrays.stream(nums).filter(n -> n%2 != 0).toArray();
-	}
-
+	/*
+	 * @param nums an array of doubles to extract odds from
+	 * @return an array of doubles containing all odd doubles in nums
+	 */
 	public static double[] getOddsFromArray(double[] nums) {
 		return Arrays.stream(nums).filter(n -> Math.abs(n%2) > 0).toArray();
 	}
--- src/main/test/com/robbins/paul/utils/JavaUtilsTests.java
+++ src/main/test/com/robbins/paul/utils/JavaUtilsTests.java
@@ -4,6 +4,7 @@ import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 import org.junit.Test;
@@ -52,16 +53,138 @@ public class JavaUtilsTests {
 
 	@Test
 	public void testTotalDoubleArray() {
-		double expectedSum = 15.0;
-		double[] doubles = new double[]{1.0,2.0,3.0,4.0,5.0};
+		double expectedSum = 17.3;
+		double[] doubles = new double[]{1.2,2.5,3.1,4.9,5.6};
 		
 		assertTrue(Math.abs(expectedSum - JavaUtils.total(doubles)) == 0);
 	}
+	
 	@Test
-	public void testTotalLongArray() {
-		long expectedSum = 15;
-		long[] longs = new long[]{1,2,3,4,5};
+	public void testIntArrayToList() {
+		int[] ints = new int[]{0,1,2,3,4,5,6,7,8,9,10};
+		List<Integer> expected = new ArrayList<>();
+		expected.add(0);
+		expected.add(1);
+		expected.add(2);
+		expected.add(3);
+		expected.add(4);
+		expected.add(5);
+		expected.add(6);
+		expected.add(7);
+		expected.add(8);
+		expected.add(9);
+		expected.add(10);
 		
-		assertEquals(expectedSum, JavaUtils.total(longs));
+		assertTrue(expected.equals(JavaUtils.arrayToList(ints)));
+	}
+
+	@Test
+	public void testDoubleArrayToList() {
+		double[] doubles = new double[]{0.0,1.0,2.0,3.0,4.0,5.0,6.0,7.0,8.0,9.0,10.0};
+		List<Double> expected = new ArrayList<>();
+		expected.add(0.0);
+		expected.add(1.0);
+		expected.add(2.0);
+		expected.add(3.0);
+		expected.add(4.0);
+		expected.add(5.0);
+		expected.add(6.0);
+		expected.add(7.0);
+		expected.add(8.0);
+		expected.add(9.0);
+		expected.add(10.0);
+		
+		assertTrue(expected.equals(JavaUtils.arrayToList(doubles)));
+	}
+	
+	@Test
+	public void testGetMultiplesInList() {
+		List<Integer> ints = JavaUtils.getRangeAsList(1,99);
+		List<Integer> expected = new ArrayList<>();
+		expected.add(13);
+		expected.add(24);
+		expected.add(26);
+		expected.add(39);
+		expected.add(48);
+		expected.add(52);
+		expected.add(65);
+		expected.add(72);
+		expected.add(78);
+		expected.add(91);
+		expected.add(96);
+
+		assertTrue(expected.equals(JavaUtils.getMultiplesInList(ints, 13, 24)));
+	}
+	
+	@Test
+	public void testIntListToArray() {
+		List<Integer> ints = new ArrayList<>();
+		ints.add(0);
+		ints.add(1);
+		ints.add(2);
+		ints.add(3);
+		ints.add(4);
+		ints.add(5);
+		ints.add(6);
+		ints.add(7);
+		ints.add(8);
+		ints.add(9);
+		ints.add(10);
+		
+		int[] expected = new int[]{0,1,2,3,4,5,6,7,8,9,10};
+		
+		assertTrue(Arrays.equals(expected, JavaUtils.intListToArray(ints)));
+	}
+
+	@Test
+	public void testDoubleListToArray() {
+		List<Double> ints = new ArrayList<>();
+		ints.add(0.0);
+		ints.add(1.0);
+		ints.add(2.0);
+		ints.add(3.0);
+		ints.add(4.0);
+		ints.add(5.0);
+		ints.add(6.0);
+		ints.add(7.0);
+		ints.add(8.0);
+		ints.add(9.0);
+		ints.add(10.0);
+		
+		double[] expected = new double[]{0.0,1.0,2.0,3.0,4.0,5.0,6.0,7.0,8.0,9.0,10.0};
+		
+		assertTrue(Arrays.equals(expected, JavaUtils.doubleListToArray(ints)));
+	}
+	
+	@Test
+	public void testIntGetEvensFromArray() {
+		int[] ints = new int[]{0,1,2,3,4,5,6,7,8,9,10};
+		int[] expected = new int[]{0,2,4,6,8,10};
+		
+		assertTrue(Arrays.equals(expected, JavaUtils.getEvensFromArray(ints)));
+	}
+
+	@Test
+	public void testDoubleGetEvensFromArray() {
+		double[] doubles = new double[]{0.0,1.0,2.0,3.0,4.0,5.0,6.0,7.0,8.0,9.0,10.0};
+		double[] expected = new double[]{0.0,2.0,4.0,6.0,8.0,10.0};
+		
+		assertTrue(Arrays.equals(expected, JavaUtils.getEvensFromArray(doubles)));
+	}
+	
+	@Test
+	public void testIntGetOddsFromArray() {
+		int[] ints = new int[]{0,1,2,3,4,5,6,7,8,9,10};
+		int[] expected = new int[]{1,3,5,7,9};
+		
+		assertTrue(Arrays.equals(expected, JavaUtils.getOddsFromArray(ints)));
+	}
+
+	@Test
+	public void testDoubleGetOddsFromArray() {
+		double[] doubles = new double[]{0.0,1.0,2.0,3.0,4.0,5.0,6.0,7.0,8.0,9.0,10.0};
+		double[] expected = new double[]{1.0,3.0,5.0,7.0,9.0};
+		
+		assertTrue(Arrays.equals(expected, JavaUtils.getOddsFromArray(doubles)));
 	}
 }
